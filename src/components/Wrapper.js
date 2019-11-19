0c19433034ca5b24a2c1c57a3928c951d362197f
Molokof <ayinde.ayobami@venturegardengroup.com>
2019-11-19 11:34:41 +0100

    initial commit

---
+++ src/components/Wrapper.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Wrapper = () => {
+  return (
+    <div>
+      <div className="container ">
+        <div className="row">
+
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Wrapper;
