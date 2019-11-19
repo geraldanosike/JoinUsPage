d525e683fe6bfe2f78a39c9bdc06215e6a50eae0
geraldanosike <geraldanosike.javhse@gmail.com>
2019-11-19 08:55:56 +0100

    Page req commit

---
+++ src/pages/join.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class JoinUs extends Component {
+    state = {  }
+    render() { 
+        return ( <div className="Header">
+            <p>HEADER</p>
+            
+        </div> );
+    }
+}
+ 
+export default JoinUs;
