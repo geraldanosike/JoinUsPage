184378abadf78c71470e745d1ef78a679c1d3f3d
geraldanosike <geraldanosike.javhse@gmail.com>
2019-11-19 12:58:42 +0100

    header done

--- src/pages/join.js
+++ src/pages/join.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
 import Nav from '../components/Nav';
 import '../css/header.css';
+import Button from "../components/Button.js"
 class JoinUs extends Component {
   render() {
     return (
-      <div>
+      <div className="">
         {/* BEGIN HEADER SECTION */}
-        <section className="Header">
+        <section className="Header container-fluid">
           <Nav JoinUs="Join Us" />
+          <div className="join">
+            <p className="join_team">Join Our Team</p>
+            <Button children="View Openings" myBtnClass="viewbtn" />
+          </div>
+        </section>
+        <section className="vgg-section">
+          <p className="vgg-section-paragraph">
+            At Venture Garden Group (VGG), the brightest minds driven by an
+            entrepreneurial spirit are transforming Africa through Technology.
+          </p>
+
+          <Button children="View Openings" myBtnClass="viewbtntwo" />
         </section>
         {/* END HEADER SECTION */}
 
