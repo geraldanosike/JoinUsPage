3f5048205da543668df69e0ad7d913cfdd986f0f
Oluwakemi Idowu <oluwakemi.idowu@VentureGardenGroup.Local>
2019-11-19 10:43:48 +0100

    initial commit

---
+++ src/Jobviews.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+class Jobview extends React.Component{
+
+    
+
+}
+
+export default Jobview;
--- src/pages/join.js
+++ src/pages/join.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Jobview from "../Jobviews";
 
 class JoinUs extends Component {
     state = {  }
     render() { 
         return ( <div className="Header">
             <p>HEADER SECTION</p>
-
+            <Jobview />
         </div> );
     }
 }
