b534bc3f33fe9d4cb247b5ff2f3a497f1dbef10e
Abayomi <omosehin14@gmail.com>
2019-11-19 10:37:47 +0100

    values

--- src/pages/join.js
+++ src/pages/join.js
@@ -7,22 +7,42 @@ class JoinUs extends Component {
       <div className="Header">
         <p>HEADER SECTION</p>
 
+        {/* Footer section */}
+
         <section className="Values">
-          <div className="container-fluid values-Container">
-            <div className="row ">
-              <div className="col-xs-12">
-                <h1 className="text-center">Our Core Values</h1>
+          <div className="container-fluid ">
+            <div className="row values-Container">
+              <div className="col-xs-12 ">
+                <h1 className="">Our Core Values</h1>
               </div>
-              <div col-lg-1> </div>
-              <div col-lg-1></div>
-              <div col-lg-1></div>
-              <div col-lg-1></div>
-              <div col-lg-1></div>
-              <div col-lg-1></div>
-              <div col-lg-1></div>
+            </div>
+            
+            <div className = "row">
+              <div className="col-lg-1">
+                <h3>Mutual Respect</h3>
+              </div>
+              <div className="col-lg-1">
+                <h3>Ingenuity</h3>
+              </div>
+              <div className="col-lg-1">
+                <h3>Ownership</h3>
+              </div>
+              <div className="col-lg-1">
+                <h3>Client Service</h3>
+              </div>
+              <div className="col-lg-1">
+                <h3>Passion</h3>
+              </div>
+              <div className="col-lg-1">
+                <h3>Integrity</h3>
+              </div>
+              <div className="col-lg-1">
+                <h3>Excellence</h3>
+              </div>
             </div>
           </div>
         </section>
+        {/* footer section ends here */}
       </div>
     );
   }
