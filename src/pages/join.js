6bb895f36e44b6f2f6e48dfcb873ed44bb6ac496
geraldanosike <geraldanosike.javhse@gmail.com>
2019-11-19 11:38:08 +0100

    diff sections addeds

--- src/pages/join.js
+++ src/pages/join.js
@@ -1,40 +1,27 @@
 import React, { Component } from "react";
-
+import Nav from '../components/Nav';
+import '../css/header.css';
 class JoinUs extends Component {
-  state = {};
   render() {
     return (
-      <div className="Header">
-        <p>HEADER SECTION</p>
+      <div>
         {/* BEGIN HEADER SECTION */}
-
+        <section className="Header">
+          <Nav JoinUs="Join Us" />
+        </section>
         {/* END HEADER SECTION */}
-
 
         {/* BEGIN OPEN POSITION SECTION */}
 
         {/* END OPEN POSITION SECTION */}
 
-
         {/* BEGIN Why You Will Love it at VGG POSITION SECTION */}
 
         {/* END Why You Will Love it at VGG POSITION SECTION */}
 
-
         {/* BEGIN FOOTER POSITION SECTION */}
 
         {/* END FOOTER POSITION SECTION */}
-
-        <section className="Values">
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-xs-12">
-                <h1>Our Core Values</h1>
-              </div>
-              <div col-lg-1></div>
-            </div>
-          </div>
-        </section>
       </div>
     );
   }
